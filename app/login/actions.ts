349e12dcd324e7c717113e709dc4821039f77e3a
Roland Hentschel <rolandhentschel@MacBook-Pro-von-Roland-2.fritz.box>
2026-01-08 21:33:37 +0100

    Improve auth error reporting

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -38,7 +38,7 @@ export async function signup(formData: FormData) {
     })
 
     if (error) {
-        return redirect('/login?message=Could not authenticate user')
+        return redirect(`/login?message=${encodeURIComponent(error.message)}`)
     }
 
     revalidatePath('/', 'layout')
